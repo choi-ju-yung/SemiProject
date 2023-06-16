ac6a46042f65d77430eb59e7d8dc16862323b4d2
choi-ju-yung <bogdudhkd123@naver.com>
2023-06-16 11:27:14 +0900

    6/16

--- semi-hifive/src/main/java/com/semi/member/service/MemberService.java
+++ semi-hifive/src/main/java/com/semi/member/service/MemberService.java
@@ -14,23 +14,23 @@ import com.semi.member.model.vo.Member;
 
 
 public class MemberService {
-	private MemberDao dao = new MemberDao();
-	
-	public Member selectByUserIdAndPw(String userId, String password) {
-		Connection conn=getConnection(); // jdbc 연결객체
-		Member m=dao.selectByUserIdAndPw(conn,userId,password);
-		close(conn);
-		return m;
-	}
-	
-	
-	public int insertMember(Member m) {
-		Connection conn = getConnection();
-		int result = dao.insertMember(conn,m);
-		if(result>0)commit(conn);
-		else rollback(conn);
-		close(conn);
-		return result;
-	}
-	
+   private MemberDao dao = new MemberDao();
+   
+   public Member selectByUserIdAndPw(String userId, String password) {
+      Connection conn=getConnection(); // jdbc 연결객체
+      Member m=dao.selectByUserIdAndPw(conn,userId,password);
+      close(conn);
+      return m;
+   }
+   
+   
+   public int insertMember(Member m) {
+      Connection conn = getConnection();
+      int result = dao.insertMember(conn,m);
+      if(result>0)commit(conn);
+      else rollback(conn);
+      close(conn);
+      return result;
+   }
+   
 }
